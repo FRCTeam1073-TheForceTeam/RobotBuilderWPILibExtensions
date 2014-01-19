42f24a4f84bd71c273926da6083d995d4b68fb9c
Evin Ugur <evinugur96@gmail.com>
2014-01-19 09:47:05 -0500

    less verbose Command.cpp

--- resources/export/cpp/Command.cpp
+++ resources/export/cpp/Command.cpp
@@ -11,28 +11,22 @@
 #end
 }
 
-// Called just before this Command runs the first time
 void #class($command.name)::Initialize() {
 	
 }
 
-// Called repeatedly when this Command is scheduled to run
 void #class($command.name)::Execute() {
 	
 }
 
-// Make this return true when this Command no longer needs to run execute()
 bool #class($command.name)::IsFinished() {
 	return false;
 }
 
-// Called once after isFinished returns true
 void #class($command.name)::End() {
 	
 }
 
-// Called when another command which requires one or more of the same
-// subsystems is scheduled to run
 void #class($command.name)::Interrupted() {
 
 }
